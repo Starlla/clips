e08d4a1da3aa7f60c5210b27463db128a3ef5c15
Claire <claire.cy.t@gmail.com>
2022-09-02 10:29:56 -0400

    handle individual modal

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -7,15 +7,15 @@ import { ModalService } from 'src/app/services/modal.service';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit {
+  @Input() modalID: string = "";
 
   constructor(public modal: ModalService) { }
 
   ngOnInit(): void {
   }
 
-  closeModal($event: Event) {
-    $event.preventDefault();
-    this.modal.toggleModal();
+  closeModal() {
+    this.modal.toggleModal(this.modalID);
   }
 
 }
