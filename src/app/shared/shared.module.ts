61d534d0b9b7ede9599fe4a65df2ab43aebdc0a9
Claire <claire.cy.t@gmail.com>
2022-09-22 12:32:35 -0400

    seperate input component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
 
 
 
@@ -10,10 +11,12 @@ import { TabComponent } from './tab/tab.component';
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    InputComponent
   ],
   exports: [ModalComponent, TabsContainerComponent, TabComponent]
 })
