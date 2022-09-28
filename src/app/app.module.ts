7e834ac20a142d0685dd46df1b25f56b81a42c66
Claire <claire.cy.t@gmail.com>
2022-09-27 23:38:53 -0400

    add angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
+import {AngularFireModule} from '@angular/fire/compat'
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,7 @@ import { NavComponent } from './nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     UserModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
