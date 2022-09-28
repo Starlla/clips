bf0067a5a8f3646567bcd80dc7402544a7cd253a
Claire <claire.cy.t@gmail.com>
2022-09-28 17:57:47 -0400

    destroy the modal

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,22 +1,23 @@
-import { Component, Input, OnInit, ElementRef } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, OnDestroy } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit {
-  @Input() modalID: string = "";
+export class ModalComponent implements OnInit, OnDestroy {
+  @Input() modalID: string = '';
 
-  constructor(public modal: ModalService, public el: ElementRef) { }
+  constructor(public modal: ModalService, public el: ElementRef) {}
 
   ngOnInit(): void {
-    document.body.appendChild(this.el.nativeElement)
+    document.body.appendChild(this.el.nativeElement);
   }
-
+  ngOnDestroy(): void {
+    document.body.removeChild(this.el.nativeElement);
+  }
   closeModal() {
     this.modal.toggleModal(this.modalID);
   }
-
 }
