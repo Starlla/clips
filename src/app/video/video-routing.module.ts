e2faf77f65613173e8b13e556b222c60f7c799d7
Claire <claire.cy.t@gmail.com>
2025-01-17 16:34:26 -0500

    add authGuardPip

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -2,21 +2,33 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
 import { UploadsComponent } from './uploads/uploads.component';
+import { AngularFireAuthModule, } from '@angular/fire/compat/auth';
+import { redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/')
 
 const routes: Routes = [
   {
     path: 'manage',
     component: ManageComponent,
     data: {
-      authOnly: true
-    }
+      authOnly: true,
+      authGuardPip: redirectUnauthorizedToHome
+    },
+    canActivate: [AngularFireAuthModule]
   },
   {
     path: 'upload',
     component: UploadsComponent,
     data: {
-      authOnly: true
-    }
+      authOnly: true,
+      authGuardPip: redirectUnauthorizedToHome
+    },
+    canActivate: [AngularFireAuthModule]
+  },
+  {
+    path: 'manage-clips',
+    redirectTo: 'manage'
   }
 
 ];
