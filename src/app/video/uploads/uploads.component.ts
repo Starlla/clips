6c1b96fcc83a21f7ea6c8019a865eb5108f73fda
Claire <claire.cy.t@gmail.com>
2025-01-17 19:16:21 -0500

    make upload two steps

--- src/app/video/uploads/uploads.component.ts
+++ src/app/video/uploads/uploads.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadsComponent implements OnInit {
   isDragover = false
+  file: File | null = null
+  nextStep = false
 
   constructor() { }
 
@@ -15,6 +17,16 @@ export class UploadsComponent implements OnInit {
 
   storeFile($event: Event) {
     this.isDragover = false;
+
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
+
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return
+    }
+
+    this.nextStep = true;
+
+    console.log(this.file)
 
   }
 
